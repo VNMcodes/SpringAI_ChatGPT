6711317b0bb53e8e91869ade9d5910ab2a04fa76
VNMcodes <jayant.singh.x7@gmail.com>
2025-07-19 13:32:19 +0530

    Initial project setup

---
+++ suggest-recipe/src/main/java/com/vnm/suggest_recipe/SuggestRecipeApplication.java
@@ -0,0 +1,13 @@
+package com.vnm.suggest_recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuggestRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuggestRecipeApplication.class, args);
+	}
+
+}
---
+++ suggest-recipe/src/test/java/com/vnm/suggest_recipe/SuggestRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vnm.suggest_recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuggestRecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
