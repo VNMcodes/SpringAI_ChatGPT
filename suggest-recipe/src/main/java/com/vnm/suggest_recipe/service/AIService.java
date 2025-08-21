3765c2700f813526ef2703ec51568f1e8ee21150
VNMcodes <jayant.singh.x7@gmail.com>
2025-08-22 01:20:09 +0530

    Final submission

--- suggest-recipe/src/main/java/com/vnm/suggest_recipe/service/AIService.java
+++ suggest-recipe/src/main/java/com/vnm/suggest_recipe/service/AIService.java
@@ -1,4 +1,42 @@
 package com.vnm.suggest_recipe.service;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
 public class AIService {
+
+    private final ChatClient chatClient;
+
+    public AIService(ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+    public String createRecipe(String ingredients,
+                               String cuisine,
+                               String dietaryRestrictions) {
+        var template = """
+                I want to create a recipe using the following ingredients: {ingredients}.
+                The cuisine type I prefer is {cuisine}.
+                Please consider the following dietary restrictions: {dietaryRestrictions}.
+                Please provide me with a detailed recipe including title, list of ingredients, and cooking instructions
+                """;
+
+        PromptTemplate promptTemplate = new PromptTemplate(template);
+        Map<String, Object> params = Map.of(
+                "ingredients", ingredients,
+                "cuisine", cuisine,
+                "dietaryRestrictions", dietaryRestrictions
+        );
+
+        Prompt prompt = promptTemplate.create(params);
+
+        return chatClient.prompt(prompt)
+                .call()
+                .content();
+    }
 }
